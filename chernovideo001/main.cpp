0fc4fed20aaa79c1a3950ed87314d31bcdc98a29
Kishan Ramesh Kumar <krameshkumar1@tudelft.nl>
2022-01-19 16:57:55 +0530

    Learning C++ jounrey begins with Cheroyoutube channel

---
+++ chernovideo001/main.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b,c;
+    string A,B,C;
+    char x=50;
+    unsigned int variable = 60;
+
+    cout<<variable;
+    variable = -10;
+    cout<<variable;
+}
---
+++ trial2/trial2v1.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+    string firstname, lastname;
+
+    cout<<"Enter first name";
+    cin>>firstname;
+    cout<<firstname;
+
+    cout<<"Hellooo";
+    return 0;
+}
